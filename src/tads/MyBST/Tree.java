6af37a124c8ab5614af5878522355ff0784e899c
bpereda <bpereda@correo.um.edu.uy>
2022-05-31 20:56:42 -0300

    insert de bst

--- src/tads/MyBST/Tree.java
+++ src/tads/MyBST/Tree.java
@@ -1,11 +1,11 @@
 package tads.MyBST;
 
-public class Tree<K extends Comparable<T>,T> implements MyTree<K,T> {
+public class Tree<K extends Comparable<K>,T> implements MyTree<K,T> {
     private Node<K,T> root;
 
     @Override
     public T find(K key) {
-        return find(key,root);
+        return find(key,root).getData();
 
     }
 
@@ -16,7 +16,7 @@ public class Tree<K extends Comparable<T>,T> implements MyTree<K,T> {
             if (key.compareTo(root.getKey())>0){
                 result = find(key,root.getRightChild());
 
-            }else if (key.compareTo(root.getKey() < 0)){
+            }else if (key.compareTo(root.getKey())<0){
                 result = find(key,root.getLeftChild());
             }
         }
@@ -25,20 +25,18 @@ public class Tree<K extends Comparable<T>,T> implements MyTree<K,T> {
     }
     @Override
     public void insert(K key, T data, K parentKey) {
-        Node<K,T> parentNode = find(parentKey);
+        Node<K,T> parentNode = find(parentKey,root);
         Node<K,T> newNode = new Node<>(key,data);
 
-        if (key.compareTo(parentKey) > 0) {
-            insert(key,data,parentKey,parentNode.getRightChild().getKey());
-
-            parentNode.setRightChild(parentNode);
-        }else if (key.compareTo()){
-
-
+        if (parentNode != null){
+            if (key.compareTo(parentKey) > 0){
+                insert(key,data,parentNode.getRightChild().getKey());
+            }else if (key.compareTo(parentKey) <= 0){
+                insert(key,data,parentNode.getLeftChild().getKey());
+            }
         }
     }
 
-    public Node<K,T> insert(K key,T data,Node<>)
 
     @Override
     public void delete(K key) {
